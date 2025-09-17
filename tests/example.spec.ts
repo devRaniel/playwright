863605ef66b014c6c7867a4220e2f20092a304c7
skyRaniel <ranielluz@skydev.solutions>
2025-09-17 15:10:39 +0800

    initial commit

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -12,14 +12,3 @@ test('get started link', async ({ page }) => {
 
   await page.waitForTimeout(5000)
 });
-
-// test('failed', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
-
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
-
-//   // Expects page to have a heading with the name of Installation.
-//   await page.waitForURL('https://playwright.dev/docs/intro')
-//   await expect(page.getByRole('heading', { name: 'Installations' })).toBeVisible();
-// });
